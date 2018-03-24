ace6a172fd76ae6775d867662d121556cff1b325
BhavyanshM <bhavyanshmishra@gmail.com>
2018-03-24 13:07:31 -0500

    Add the code for Content Provider

--- Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -25,7 +25,7 @@ import android.support.annotation.NonNull;
 // TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
-
+    private TaskDbHelper mDbHelper;
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
     In this case, you’re working with a SQLite database, so you’ll need to
@@ -33,9 +33,9 @@ public class TaskContentProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
+        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startupf
         // [Hint] Declare the DbHelper as a global variable
-
+        mDbHelper = new TaskDbHelper(getContext());
         return false;
     }
 
